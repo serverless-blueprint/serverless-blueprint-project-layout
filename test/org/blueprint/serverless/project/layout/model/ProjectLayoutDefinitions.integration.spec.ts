6a15a54ac85167c7c7920dae8829c36e2191bf31
sarthakmakhija <sarthak.makhija@gmail.com>
2019-12-03 23:20:11 +0530

    Sarthak | Adds integration tests for ProjectLayoutDefinitions, currently this integration tests runs against source resources

---
+++ test/org/blueprint/serverless/project/layout/model/ProjectLayoutDefinitions.integration.spec.ts
@@ -0,0 +1,18 @@
+import {ProjectLayoutType} from "../../../../../../../src/org/blueprint/serverless/project/layout/model/ProjectLayoutType";
+import {ProjectLayoutDefinitions} from "../../../../../../../src/org/blueprint/serverless/project/layout/model/ProjectLayoutDefinitions";
+import {ProjectLayoutDefinition} from "../../../../../../../src/org/blueprint/serverless/project/layout/model/ProjectLayoutDefinition";
+
+import {expect} from "chai";
+
+describe("Project Layout Definitions (Integration)", () => {
+
+    it("should load ProjectLayoutDefinition a nested layout type", () => {
+
+        let projectLayoutDefinition: ProjectLayoutDefinition = new ProjectLayoutDefinitions().findBy(ProjectLayoutType.Nested);
+
+        expect(projectLayoutDefinition.projectName).to.equal("serverless-blueprint");
+        expect(projectLayoutDefinition.hierarchyPaths()).to.deep.equal([
+            "serverless-blueprint/src/serverless/controller",
+            "serverless-blueprint/test/serverless/controller"]);
+    });
+});
